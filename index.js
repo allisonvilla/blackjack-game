0ddc630d7863e7df8cd8372d02d1e876ebac0a88
allison <allison.villa@outlook.com>
2022-01-30 23:12:45 -0500

    added comments for later studying

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// Drawing cards
 let firstCard = Math.floor(Math.random() * (12 - 2) + 2); 
 let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
 let sum = firstCard + secondCard; 
+
+// Initializing values
 let hasBlackjack = false; 
 let isAlive = true; 
 let message = ""; 
 
+// Clicking "Start Game" calls on startGame()
 document.getElementById("start-btn").addEventListener("click", startGame); 
 
+// Storing html elements in variables
 let messageEl = document.querySelector("#message-el"); 
 let sumEl = document.querySelector("#sum-el"); 
 let cardsEl = document.querySelector("#cards-el"); 
@@ -29,4 +34,11 @@ function startGame() {
     console.log(sum);
     console.log(hasBlackjack);
     console.log(isAlive);
+}
+
+// Clicking "New Card" calls on newCard()
+document.querySelector("#new-btn").addEventListener("click", newCard); 
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
 }
