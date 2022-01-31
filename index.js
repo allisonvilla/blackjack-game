86750fbfaeebb70f4bf5eb01e29cfa1e45ed1a04
allison <allison.villa@outlook.com>
2022-01-30 21:08:01 -0500

    added random numbers and win/lose conditions

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let firstCard = Math.floor(Math.random() * (12 - 2) + 2); 
+console.log(firstCard); 
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
+console.log(secondCard); 
+
+let sum = firstCard + secondCard; 
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+    console.log("You win!");
+} else {
+    console.log("You lose!"); 
+}
