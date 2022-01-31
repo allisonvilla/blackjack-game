718cbebce1e5d15b31dd106cbd021025770220aa
allison <allison.villa@outlook.com>
2022-01-30 22:45:45 -0500

    added start button and display sum

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 let firstCard = Math.floor(Math.random() * (12 - 2) + 2); 
-console.log(firstCard); 
-
 let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
-console.log(secondCard); 
-
 let sum = firstCard + secondCard; 
+let hasBlackjack = false; 
+let isAlive = true; 
+let message = ""; 
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?");
-} else if (sum === 21) {
-    console.log("You win!");
-} else {
-    console.log("You lose!"); 
+document.getElementById("start-btn").addEventListener("click", startGame); 
+
+let messageEl = document.getElementById("message-el"); 
+let sumEl = document.getElementById("sum-el"); 
+
+function startGame() {
+    sumEl.textContent = `Sum: ${sum}`; 
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got blackjack!";
+        hasBlackjack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = `${message}`; 
+
+    console.log(message);
+    console.log(sum);
+    console.log(hasBlackjack);
+    console.log(isAlive);
 }
